7e94863b1ff0be111675cacb9cf6e94bffedf7ba
SergenEsendemir <sergen_35_96@hotmail.com>
2022-01-11 17:07:45 +0300

    first commit

---
+++ src/components/validations.js
@@ -0,0 +1,13 @@
+import { object, string, number, date, InferType, ref } from "yup";
+
+const validations = object({
+  email: string().email("Geçerli bir email girin.").required("Zorunlu alan."),
+  password: string()
+    .min(5, "Parolanız en az 5 karakter olmalıdır.")
+    .required("Zorunlu alan."),
+  passwordConfirm: string()
+    .oneOf([ref("password")], 'Parolalar uyuşmuyor.')
+    .required("Zorunlu alan."),
+});
+
+export default validations;
